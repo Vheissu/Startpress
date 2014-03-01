a1fc8674b23b84a33913522411b30b841500790e
Dwayne Charrington <dwaynecharrington@gmail.com>
2014-03-01 21:19:55 +1000

    Nobody uses the custom header feature, just delete the files man.

--- functions.php
+++ functions.php
@@ -137,11 +137,6 @@ function theme_scripts()
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
