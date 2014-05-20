8ebffbf48b7f1d7e4314b946ebdb6ec72e15ffaf
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-20 16:51:05 +1000

    Link to documentation and example starting function for taxonomies.

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1,3 +1,10 @@
 <?php
 
-// Custom taxonomies
+/* Register custom taxonomies in here */
+
+add_action("init", "register_custom_taxonomy");
+
+function register_custom_taxonomy()
+{
+    // http://codex.wordpress.org/Function_Reference/register_taxonomy
+}
