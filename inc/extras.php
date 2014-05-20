b6cb285a7bc714d82d1fc62e439201aa20e3f283
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-20 16:53:53 +1000

    Fixing up includes folder and extras file.

--- inc/extras.php
+++ inc/extras.php
@@ -7,39 +7,43 @@
  * @package Theme
  */
 
-/* Function code courtesy of Brian Cray: http://briancray.com/posts/estimated-reading-time-web-design/ */
-function theme_average_reading_time()
+if ( ! function_exists("theme_average_reading_time") )
 {
-    global $post;
-    $content = $post->post_content;
+    /* Function code courtesy of Brian Cray: http://briancray.com/posts/estimated-reading-time-web-design/ */
+    function theme_average_reading_time()
+    {
+        global $post;
+        $content = $post->post_content;
 
-    $word = str_word_count(strip_tags($content));
-    $m = floor($word / 200);
-    $s = floor($word % 200 / (200 / 60));
-    $est = $m . ' minute' . ($m == 1 ? '' : 's') . ', ' . $s . ' second' . ($s == 1 ? '' : 's');
+        $word = str_word_count(strip_tags($content));
+        $m = floor($word / 200);
+        $s = floor($word % 200 / (200 / 60));
+        $est = $m . ' minute' . ($m == 1 ? '' : 's') . ', ' . $s . ' second' . ($s == 1 ? '' : 's');
 
-    return $est;
+        return $est;
+    }
 }
 
 /**
  * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
  */
-function theme_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
+function theme_page_menu_args( $args )
+{
+    $args['show_home'] = true;
+    return $args;
 }
 add_filter( 'wp_page_menu_args', 'theme_page_menu_args' );
 
 /**
  * Adds custom classes to the array of body classes.
  */
-function theme_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+function theme_body_classes( $classes )
+{
+    if ( is_multi_author() ) {
+        $classes[] = 'group-blog';
+    }
 
-	return $classes;
+    return $classes;
 }
 add_filter( 'body_class', 'theme_body_classes' );
 
@@ -47,24 +51,24 @@ add_filter( 'body_class', 'theme_body_classes' );
  * Filters wp_title to print a neat <title> tag based on what is being viewed.
  */
 function theme_wp_title( $title, $sep ) {
-	global $page, $paged;
+    global $page, $paged;
 
-	if ( is_feed() )
-		return $title;
+    if ( is_feed() )
+        return $title;
 
-	// Add the blog name
-	$title .= get_bloginfo( 'name' );
+    // Add the blog name
+    $title .= get_bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title .= " $sep $site_description";
+    // Add the blog description for the home/front page.
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+        $title .= " $sep $site_description";
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		$title .= " $sep " . sprintf( __( 'Page %s', 'wptheme' ), max( $paged, $page ) );
+    // Add a page number if necessary:
+    if ( $paged >= 2 || $page >= 2 )
+        $title .= " $sep " . sprintf( __( 'Page %s', 'wptheme' ), max( $paged, $page ) );
 
-	return $title;
+    return $title;
 }
 add_filter( 'wp_title', 'theme_wp_title', 10, 2 );
 
@@ -73,7 +77,7 @@ add_filter( 'wp_title', 'theme_wp_title', 10, 2 );
  */
 function cc_mime_types( $mimes )
 {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
